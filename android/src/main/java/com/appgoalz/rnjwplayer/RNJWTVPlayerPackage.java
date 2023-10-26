065717680cf4938b833b00af11cb48ef62721fa5
babinTringapps <148857983+babinTringapps@users.noreply.github.com>
2023-10-26 16:41:20 +0530

    Update and rename RNJWPlayerPackage.java to RNJWTVPlayerPackage.java

--- android/src/main/java/com/appgoalz/rnjwplayer/RNJWTVPlayerPackage.java
+++ android/src/main/java/com/appgoalz/rnjwplayer/RNJWTVPlayerPackage.java
@@ -11,7 +11,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public class RNJWPlayerPackage implements ReactPackage {
+public class RNJWTVPlayerPackage implements ReactPackage {
 
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
